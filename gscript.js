9bd709c59dd9e6231ab9334374ea14255f06454e
Fellintron <fellintron@gmail.com>
2023-03-08 20:57:26 +0530

    basic code

---
+++ gscript.js
@@ -0,0 +1,31 @@
+function onSubmit(e) {
+    try {
+      var form = FormApp.getActiveForm();
+      var responses = form.getResponses();
+      var lastResponse = responses[responses.length - 1];
+      var response = lastResponse.getItemResponses();
+      var formData = {};
+  
+      for (var i = 0; i < response.length; i++) {
+        formData[response[i].getItem().getTitle()] = response[i].getResponse();
+      }
+  
+      var data = {
+        "content": "New form submission:\n" + JSON.stringify(formData)
+      };
+  
+      var options = {
+        "method": "post",
+        "payload": JSON.stringify(data),
+        "contentType": "application/json"
+      };
+  
+      var webhookUrl = "WEBHOOK_URL_HERE"; // Please make sure you input the discord.com url and exclude canary.discord/ptb.discord or any other client
+      UrlFetchApp.fetch(webhookUrl, options);
+      
+      Logger.log("Posted the form!");
+    } catch (error) {
+      Logger.log("Can't post, help: " + error); 
+    }
+  }
+  
